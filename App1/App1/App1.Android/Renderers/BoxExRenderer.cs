e4cee52c3ae413634b942a7b97b6d5aab1c10660
向田義和 <yo-mukaida@kentem.co.jp>
2019-05-06 19:04:48 +0900

    拡大縮小を実装　拡大するとうまく動かないけど

--- App1/App1/App1.Android/Renderers/BoxExRenderer.cs
+++ App1/App1/App1.Android/Renderers/BoxExRenderer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Android.Content;
+using Android.Graphics;
 using Android.Runtime;
 using Android.Views;
 using App1;
@@ -18,8 +19,10 @@ namespace App1.Droid.Renderers
 {
     public class BoxExRenderer : BoxRenderer
     {
+        private const double ScaleChangeWidth = 5;
         private float _prevX;
         private float _prevY;
+        private Directions _scaleChangeDirections = Directions.None;
         private Rectangle _downRect;
 
         public BoxExRenderer(Context context)
@@ -33,25 +36,60 @@ namespace App1.Droid.Renderers
             this.Touch += BoxExRenderer_Touch;
         }
 
+        public override void Draw(Canvas canvas)
+        {
+            base.Draw(canvas);
+        }
+
         private void BoxExRenderer_Touch(object sender, TouchEventArgs e)
         {
             var box = (BoxViewEx)Element;
+            var area = (BoxAreaLayout)box.Parent.Parent;
+            var scale = area.Scale;
             switch (e.Event.Action)
             {
                 case MotionEventActions.Down:
                     box.GetParent<BoxAreaLayout>().ResetAllSelect();
                     box.Selected = true;
+
+                    var xOnBox = e.Event.GetX();
+                    var yOnBox = e.Event.GetY();
+                    if (xOnBox < ScaleChangeWidth)
+                        _scaleChangeDirections |= Directions.Left;
+                    else if (xOnBox - ScaleChangeWidth > box.Width * 2)
+                        _scaleChangeDirections |= Directions.Right;
+                    if (yOnBox < ScaleChangeWidth)
+                        _scaleChangeDirections |= Directions.Top;
+                    else if (yOnBox - ScaleChangeWidth > box.Width * 2)
+                        _scaleChangeDirections |= Directions.Bottom;
+
                     _downRect = box.ViewModel.GetRect();
+                    var s1 = ScaleX;
+                    var s2 = ScaleXs;
+                    var s3 = ScaleY;
+                    var s4 = ScaleYs;
+                    System.Diagnostics.Debug.WriteLine("   xxx");
+                    System.Diagnostics.Debug.WriteLine($"box x:{box.ViewModel.X}, y:{box.ViewModel.Y}, width:{box.ViewModel.Width}, height:{box.ViewModel.Height}");
+                    System.Diagnostics.Debug.WriteLine($"frame x:{((Frame)box.Parent).X}, y:{((Frame)box.Parent).Y}, width:{((Frame)box.Parent).Width}, height:{((Frame)box.Parent).Height}");
+                    System.Diagnostics.Debug.WriteLine($"touch x:{e.Event.RawX}, y:{e.Event.RawY}");
+                    System.Diagnostics.Debug.WriteLine($"touch x:{e.Event.GetX()}, y:{e.Event.GetY()}");
                     break;
                 case MotionEventActions.Move:
                     var x = (e.Event.RawX - _prevX) / 3;
                     var y = (e.Event.RawY - _prevY) / 3;
+                    if(!_scaleChangeDirections.HasFlag(Directions.Right))
                     box.ViewModel.X += x;
-                    box.ViewModel.Y += y;
+                    if (!_scaleChangeDirections.HasFlag(Directions.Bottom))
+                        box.ViewModel.Y += y;
+                    if (_scaleChangeDirections.HasFlag(Directions.Left) || _scaleChangeDirections.HasFlag(Directions.Right))
+                        box.ViewModel.Width += x;
+                    if (_scaleChangeDirections.HasFlag(Directions.Top) || _scaleChangeDirections.HasFlag(Directions.Bottom))
+                        box.ViewModel.Height += y;
                     box.UpdateLocationAndSize();
                     break;
                 case MotionEventActions.Up:
                 case MotionEventActions.Cancel:
+                    _scaleChangeDirections = Directions.None;
                     UndoManager.Push(() =>
                     {
                         box.ViewModel.X = _downRect.X;
---
+++ App1/App1/App1/Models/Directions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    public enum Directions
+    {
+        None,
+        Left = 1,
+        Top = 2,
+        Right = 4,
+        Bottom = 8,
+    }
+}
